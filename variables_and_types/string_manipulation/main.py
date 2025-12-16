b3a9f5d7c20c2e35c7cd309b22d196e6703b65e7
Yuanzhenwei123 <>
2025-12-16 16:24:28 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,10 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+words = grocery_items.split()
+dairy1 = words[0]
+dairy2 = words[1]
+bakery1 = words[2]
+# print(f"We have dairy and bakery items:{dairy1}, {dairy2}, and {bakery1} in aisle 5")
+print("We have dairy and bakery items: " 
+      + dairy1 + ", " 
+      + dairy2 + ", and " 
+      + bakery1 + " in aisle 5")
