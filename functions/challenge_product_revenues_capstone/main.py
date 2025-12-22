210b581cb46101a7507df7ca469e6437fd14bd84
Yuanzhenwei123 <>
2025-12-22 16:07:06 +0000

    Challenge: Product Revenues Capstone

--- functions/challenge_product_revenues_capstone/main.py
+++ functions/challenge_product_revenues_capstone/main.py
@@ -4,10 +4,11 @@ prices = [0.50, 1.20, 2.50, 2.00]  # price per item
 quantities_sold = [150, 200, 100, 50]  # number of items sold
 
 def calculate_revenue(prices, quantities_sold):
-    revenue = []
-    for i in range(len(prices)):
-        revenue.append(prices[i]*quantities_sold[i])
-    return revenue
+    # revenue = []
+    # for i in range(len(prices)):
+        # revenue.append(prices[i]*quantities_sold[i])
+    # return revenue
+    return [p * q for p, q in zip(prices, quantities_sold)]
 
 def formatted_output(revenues):
     for product,rev in sorted(revenues):
